b0e3ea9e3706d7ae3de32ec86681379301df8a1e
Jerome <jerome.coubronne@cgi.com>
2018-05-09 12:02:11 +0200

    Création de l'entity Review

---
+++ ChasseAuTresor/src/Entity/Review.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ReviewRepository")
+ */
+class Review
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $review;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getReview(): ?string
+    {
+        return $this->review;
+    }
+
+    public function setReview(?string $review): self
+    {
+        $this->review = $review;
+
+        return $this;
+    }
+}
