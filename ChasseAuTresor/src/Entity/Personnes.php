0c6424a706078116634a4cc7497100d1afa1306d
Jcoub1 <jerome.coubronne@gmail.com>
2018-05-22 17:14:40 +0200

    Ajout security.yaml / login controller

--- ChasseAuTresor/src/Entity/Personnes.php
+++ ChasseAuTresor/src/Entity/Personnes.php
@@ -5,11 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PersonnesRepository")
  */
-class Personnes
+class Personnes implements UserInterface, \Serializable
 {
     /**
      * @ORM\Id()
@@ -27,6 +28,11 @@ class Personnes
      * @ORM\Column(type="string", length=25)
      */
     private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="string", length=25)
@@ -238,5 +244,52 @@ class Personnes
         }
 
         return $this;
+    }
+
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+            ) = unserialize($serialized, ['allowed_classes' => false]);
+    }
+
+    public function getRoles()
+    {
+        return array("ROLE_USER");
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getUsername()
+    {
+        return $this->pseudo;
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
