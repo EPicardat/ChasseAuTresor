e7d0b39a2c71401d9e9051cb92e2c3a05f1b7a7d
EPicardat <mmeemorin@gmail.com>
2018-05-23 10:18:27 +0200

    position orthodromique

--- ChasseAuTresor/src/Controller/ApiCatController.php
+++ ChasseAuTresor/src/Controller/ApiCatController.php
@@ -66,7 +66,7 @@ class ApiCatController extends Controller
         // on enregistre les coordonnées dans la la table propositionGPS
         $this->setLocGPS($request);
 
-        // On compare ces coordonnées avec les coordonnées solutions de la partie
+        // On compare ces coordonnées avec les coordonnées solutions de la partie (id partie)
         $found = $this->compareLocGPS($request);
 
         return $found;
@@ -86,10 +86,66 @@ class ApiCatController extends Controller
 
         // on récupère les coordonnées solutions
 
-        // on les compare avec celles soumises
+        $partieRepo = $this->getDoctrine()->getRepository(Parties::class);
+        $q =$request->query->get('q');
+        $partie=$partieRepo->find($q);
 
-        // on renvoie un booléen true/false en fonction du résultat de la comparaison
+        $latitudeSolution = $partie->getLatitude();
+        $longitudeSolution = $partie->getLongitude();
+        $accuracySolution = $partie->getAccuracy();
+
+        // on récupère les coordonnées soumises
+
+        $latitudeSoumise = ;
+        $longitudeSoumise = ;
+        $accuracySoumise = ;
+
+        // on compare les deux accuracies. On travaille avec la plus grandes des deux.
+        $accuracyUtile = $accuracySoumise;
+        if($accuracySolution>$accuracySoumise){
+            $accuracyUtile = $accuracySolution;
+        }
+
+        // on récupère la distance à vol d'oiseau en mètre (orthodromique) entre la position soumise et la position solution
+
+        $d = distance_orthodromique($latitudeSoumise, $latitudeSolution, $longitudeSoumise, $longitudeSolution);
+
+        // Si la $d est inférieure ou égale à l'accuracy utile, le joueur à bien trouvé le lieu du trésor : on passe le booléen found à true
+        if ($d<= $accuracyUtile){
+            $found = true;
+        }
+
+        // on renvoie le booléen $found
 
         return $found;
+    }
+
+    private function distance_orthodromique($latitudeSoumise, $latitudeSolution, $longitudeSoumise, $longitudeSolution, $precision = 3, $r = 6378.14)
+    {
+        // La variable $r correspond au rayon de la Terre.
+        // $latitudeSoumise, $longitudeSoumise sont les latitudes de chaques points respectifs.
+        // $latitudeSolution, $y$longitudeSolution2 sont les longitudes de chaques points respectifs.
+        // $precision permet d'obtenir le nombre de chiffre après la virgule.
+        // Elle est définit à 3 par défaut permettant d'obtenir une précision au mètre. Il vous suffira de la multiplier par 1000.
+
+        // On convertit les latitudes et longitudes en radian.
+        $latitudeSoumise = deg2rad($latitudeSoumise);
+        $longitudeSoumise = deg2rad($longitudeSoumise);
+        $latitudeSolution = deg2rad($latitudeSolution);
+        $longitudeSolution = deg2rad($longitudeSolution);
+
+        // On calcule des distances entre les deux points.
+        $dlat = $longitudeSoumise - $latitudeSoumise;
+        $dlong = $longitudeSolution - $latitudeSolution;
+
+        // On applique la formule.
+        $a = sin($dlat/2)*sin($dlat/2) + cos($latitudeSoumise)*cos($longitudeSoumise)*sin($dlong/2)*sin($dlong/2);
+        $c = 2 * atan2(sqrt($a), sqrt(1-$a));
+
+        // On récupère la valeur du résutat arrondi avec la précision.
+        $d =  round($r*$c,  $precision)*1000;
+
+        // On renvoie la distance en m
+        return $d;
     }
 }
