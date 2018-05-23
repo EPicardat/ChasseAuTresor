798241c2ce2c8bcf918ce18a657becb05322e357
EPicardat <mmeemorin@gmail.com>
2018-05-23 12:00:50 +0200

    modif mineures

--- ChasseAuTresor/src/Controller/ApiCatController.php
+++ ChasseAuTresor/src/Controller/ApiCatController.php
@@ -34,14 +34,14 @@ class ApiCatController extends Controller
      */
     public function setPartie(Request $request)
     {
-        //On crée une instance de partie vide
+        // On crée une instance de partie vide
         $partie=new Parties();
-        //On crée le formulaire et lui associe notre instance de partie vide
+        // on crée le formulaire et lui associe notre instance de partie vide
         $form=$this->createForm(Parties::class,$partie);
-        //On prend les données du formulaire et les injecte dans la partie vide
+        // On prend les données du formulaire et les injecte dans la partie vide
         $form->handleRequest($request);
 
-        //On récupère et set la date actuelle (== date début de partie)
+        // On récupère et set la date actuelle (== date début de partie)
         $partie->setDateDebut(new \DateTime());
 
         if($form->isSubmitted()&& $form->isValid()){
@@ -61,72 +61,63 @@ class ApiCatController extends Controller
     /**
      * @Route("/apiCat/v1/submitLocGPS", name="submit_loc_gps", methods={"GET"})
      */
-    public function submitLocGPS(Request $request)
+    public function submitLocGPS(Request $request, $latitudeSoumise, $longitudeSoumise, $accuracySoumise)
     {
-        // on enregistre les coordonnées dans la la table propositionGPS
-        $this->setLocGPS($request);
+        // On enregistre les coordonnées dans la la table propositionGPS
+        $this->setLocGPS($request, $latitudeSoumise, $longitudeSoumise, $accuracySoumise);
 
         // On compare ces coordonnées avec les coordonnées solutions de la partie (id partie)
-        $found = $this->compareLocGPS($request);
+        $found = $this->compareLocGPS($request, $latitudeSoumise, $longitudeSoumise, $accuracySoumise);
 
         return $found;
     }
 
-    private function setLocGPS(Request $request)
+    private function setLocGPS(Request $request, $latitudeSoumise, $longitudeSoumise, $accuracySoumise)
     {
     //TODO
     }
 
-    private function compareLocGPS(Request $request)
+    private function compareLocGPS(Request $request, $latitudeSoumise, $longitudeSoumise, $accuracySoumise)
     {
     //TODO
 
-        //On set le booléen à faux par défault
+        // On set le booléen à faux par défault
         $found = false;
 
-        // on récupère les coordonnées solutions
-
+        // On récupère les coordonnées solutions
         $partieRepo = $this->getDoctrine()->getRepository(Parties::class);
-        $q =$request->query->get('q');
-        $partie=$partieRepo->find($q);
+        $id =$request->query->get('id');
+        $partie=$partieRepo->find($id);
 
         $latitudeSolution = $partie->getLatitude();
         $longitudeSolution = $partie->getLongitude();
         $accuracySolution = $partie->getAccuracy();
 
-        // on récupère les coordonnées soumises
-
-        $latitudeSoumise = ;
-        $longitudeSoumise = ;
-        $accuracySoumise = ;
-
-        // on compare les deux accuracies. On travaille avec la plus grandes des deux.
+        // On compare les deux accuracies. On travaille avec la plus grandes des deux.
         $accuracyUtile = $accuracySoumise;
         if($accuracySolution>$accuracySoumise){
             $accuracyUtile = $accuracySolution;
         }
 
-        // on récupère la distance à vol d'oiseau en mètre (orthodromique) entre la position soumise et la position solution
-
+        // On récupère la distance à vol d'oiseau en mètre (orthodromique) entre la position soumise et la position solution
         $d = distance_orthodromique($latitudeSoumise, $latitudeSolution, $longitudeSoumise, $longitudeSolution);
 
-        // Si la $d est inférieure ou égale à l'accuracy utile, le joueur à bien trouvé le lieu du trésor : on passe le booléen found à true
+        // Si la $d est inférieure ou égale à l'accuracy utile, le joueur a bien trouvé le lieu du trésor : on passe le booléen $found à true
         if ($d<= $accuracyUtile){
             $found = true;
         }
 
-        // on renvoie le booléen $found
-
+        // On renvoie le booléen $found
         return $found;
     }
 
     private function distance_orthodromique($latitudeSoumise, $latitudeSolution, $longitudeSoumise, $longitudeSolution, $precision = 3, $r = 6378.14)
     {
         // La variable $r correspond au rayon de la Terre.
-        // $latitudeSoumise, $longitudeSoumise sont les latitudes de chaques points respectifs.
-        // $latitudeSolution, $y$longitudeSolution2 sont les longitudes de chaques points respectifs.
+        // $latitudeSoumise, $longitudeSoumise sont les latitudes des points respectifs.
+        // $latitudeSolution, $y$longitudeSolution sont les longitudes des points respectifs.
         // $precision permet d'obtenir le nombre de chiffre après la virgule.
-        // Elle est définit à 3 par défaut permettant d'obtenir une précision au mètre. Il vous suffira de la multiplier par 1000.
+        // Elle est définie à 3 par défaut permettant d'obtenir une précision au mètre. Il suffit de la multiplier par 1000.
 
         // On convertit les latitudes et longitudes en radian.
         $latitudeSoumise = deg2rad($latitudeSoumise);
@@ -145,7 +136,7 @@ class ApiCatController extends Controller
         // On récupère la valeur du résutat arrondi avec la précision.
         $d =  round($r*$c,  $precision)*1000;
 
-        // On renvoie la distance en m
+        // On renvoie la distance en mètres
         return $d;
     }
 }
